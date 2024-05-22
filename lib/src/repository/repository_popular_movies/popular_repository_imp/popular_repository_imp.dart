3d81a0968ee2c6f951ee8b1cfa2d403ce793bd66
mohamed <esammww.com@gmail.com>
2024-05-22 21:14:52 +0300

    create file dependency injection

--- lib/src/repository/repository_popular_movies/popular_repository_imp/popular_repository_imp.dart
+++ lib/src/repository/repository_popular_movies/popular_repository_imp/popular_repository_imp.dart
@@ -1,7 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:movies_app/src/data/model/response/movie_respons_dto.dart';
 import 'package:movies_app/src/helper/failure.dart';
-import 'package:movies_app/src/repository/repository_popular_movies/popular_remote_datasource_imp/popular_remote_datasource_imp.dart';
 import 'package:movies_app/src/repository/repository_popular_movies/popular_repository_contract.dart';
 
 class PopularRepositoryImp implements PopularRepository {
@@ -12,8 +11,4 @@ class PopularRepositoryImp implements PopularRepository {
   Future<Either<Failure, MovieResponseDto>> getPopularMovies() async {
     return await popularRemoteDataSource.getPopularMovies();
   }
-}
-
-PopularRepository injectPopularRepository() {
-  return PopularRepositoryImp(injectPopularRemoteDataSource());
 }
