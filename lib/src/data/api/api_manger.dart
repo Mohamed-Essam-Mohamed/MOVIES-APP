b128ab5df3fba540e34a59a98ea3dc97f18c2796
mohamed <esammww.com@gmail.com>
2024-05-25 19:20:39 +0300

    added function movie details and similar movies

--- lib/src/data/api/api_manger.dart
+++ lib/src/data/api/api_manger.dart
@@ -2,10 +2,11 @@ import 'dart:convert';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dartz/dartz.dart';
-import '../../constants/app_api_const.dart';
-import '../model/response/movie_respons_dto.dart';
-import '../../helper/failure.dart';
 import 'package:http/http.dart' as http;
+import 'package:movies_app/src/constants/app_api_const.dart';
+
+import '../../helper/failure.dart';
+import '../model/response/movie_respons_dto.dart';
 
 class ApiManger {
   ApiManger._();
@@ -67,6 +68,50 @@ class ApiManger {
       Uri url = Uri.https(apiBaseUrl, apiRecommendEndPoint, {
         "api_key": apiKey,
       });
+      var response = await http.get(url);
+      var popularResponse =
+          MovieResponseDto.fromJson(jsonDecode(response.body));
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+        return Right(popularResponse);
+      } else {
+        return Left(ServerFailure(errorMessage: popularResponse.statusMessage));
+      }
+    } else {
+      return Left(NetworkFailure(errorMessage: "No internet connection"));
+    }
+  }
+
+  Future<Either<Failure, MovieResponseDto>> getMovieDetails(
+      {required int movieId}) async {
+    final List<ConnectivityResult> connectivityResult =
+        await (Connectivity().checkConnectivity());
+    if (connectivityResult.contains(ConnectivityResult.mobile) ||
+        connectivityResult.contains(ConnectivityResult.wifi)) {
+      Uri url = Uri.parse(
+          "https://${apiBaseUrl}${apiMovieEndPoint}/${movieId}?api_key=${apiKey}");
+
+      var response = await http.get(url);
+      var popularResponse =
+          MovieResponseDto.fromJson(jsonDecode(response.body));
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+        return Right(popularResponse);
+      } else {
+        return Left(ServerFailure(errorMessage: popularResponse.statusMessage));
+      }
+    } else {
+      return Left(NetworkFailure(errorMessage: "No internet connection"));
+    }
+  }
+
+  Future<Either<Failure, MovieResponseDto>> getSimilarMovies(
+      {required int movieId}) async {
+    final List<ConnectivityResult> connectivityResult =
+        await (Connectivity().checkConnectivity());
+    if (connectivityResult.contains(ConnectivityResult.mobile) ||
+        connectivityResult.contains(ConnectivityResult.wifi)) {
+      Uri url = Uri.parse(
+          "https://${apiBaseUrl}${apiMovieEndPoint}/${movieId}/similar?api_key=${apiKey}");
+
       var response = await http.get(url);
       var popularResponse =
           MovieResponseDto.fromJson(jsonDecode(response.body));
